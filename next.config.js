c947f4bf72bc671dcdb9a060a4b0e4ba252e10de
umer-iq <umeritsolutions@gmail.com>
2022-08-04 22:03:11 +0500

    add

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript:{
+
   ignoreBuildErrors : true,},
   reactStrictMode: true,
   swcMinify: true,
+  
   images:{
     domains: ['i.ibb.co','lh3.googleusercontent.com'],
   }
